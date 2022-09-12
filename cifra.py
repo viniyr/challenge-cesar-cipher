d2a8a4af41e6b2cde1627981c5bba33c45b685c5
gilmartinsjr <113190905+gilmartinsjr@users.noreply.github.com>
2022-09-12 14:15:53 -0300

    Update cifra.py
    Proposta do grupo 7 de alterações no código do grupo 8.

--- cifra.py
+++ cifra.py
@@ -1,16 +1,16 @@
 posicao = int(input("Qual a chave?"))
 frase = input("Diga uma frase para ser criptografada: ")
-lista_frase = list(frase)
-new_frase_lista = []
-for letra in lista_frase:
-    asc2 = ord(letra)
-    if asc2 != 32:
-        if(asc2 == 121):
-            asc2 = 97
-        elif(asc2 == 122):
-           asc2 = 98
-        else: 
-            asc2 += posicao
-    new_frase_lista.append(chr(asc2).upper())
-new_frase = "".join(new_frase_lista)
+alfabeto = "abcdefghijklmnopqrstuvwxyz" #criei alfabeto em string
+frase = frase.lower() #deixei a frase em letra minuscula
+new_frase = "" #criei uma string para adicionar a nova frase
+for letra in frase:
+    if letra != ' ':
+        posicao_caractere = alfabeto.index(letra) #acha a posição da letra no alfabeto
+        if(posicao_caractere > (25-posicao)):
+            posicao_caractere = (posicao-1)-(25-posicao_caractere) #substitui a posicao do caractere considerando a posição da letra em relação à chave
+        else:
+            posicao_caractere += posicao
+        new_frase = new_frase + alfabeto[posicao_caractere] #adiciona a letra criptografada na string
+    if letra == ' ':
+        new_frase = new_frase + " " #adiciona os espaços
 print("Frase Criptografada: ", new_frase)
